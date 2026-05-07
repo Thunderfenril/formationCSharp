2b8c7513a457f8e67ddd7ee221d292a8e4a05137
FORMATION <FORMATION@LAPTOP-H6GDXD3>
2026-05-07 15:30:13 +0200

    Séparation fonctions entrées

--- Banque/Entree.cs
+++ Banque/Entree.cs
@@ -8,6 +8,13 @@ using System.Threading.Tasks;
 
 namespace Banque
 {
+    public struct EntreeCompteStruct {
+        bool statut;
+        int id;
+        long idCarte;
+        string type;
+        decimal soldeInit;
+    }
     public class Entree
     {
         /// <summary>
@@ -25,60 +32,19 @@ namespace Banque
                 using(StreamReader reader= new StreamReader(file))
                 {
                     string line;
-                    string[] data;
-                    int id;
-                    long idCarte;
-                    string type;
-                    decimal soldeInit;
 
                     while((line = reader.ReadLine()) != null)
                     {
-                        data = line.Split(';');
 
-                        
+                        (bool, int, long, string, decimal) compteVerif = CompteVerification(line, res);
 
-                        if(data.Length > 2) // Vérification que l'on a assez de données
+                        if (compteVerif.Item1 != false)
                         {
-
-                            id = int.Parse(data[0]);
-                            idCarte = long.Parse(data[1]);
-                            type = data[2];
-
-                            if (!res.ContainsKey(id) && idCarte.ToString().Length == 16) // Vérification que l'on n'a pas encore le compte et que son id soit assez long
-                            {
-                                if(type.ToLower() == "livret" || type.ToLower() == "courant") // Vérification que le type de comtpe soit correct.
-                                {
-
-                                    if (data.Length > 3) // Mise en place de la variable soldeInit
-                                    {
-                                        if (data[3].Contains(',')) //Vérification que le montant initial si il existe, n'a pas de ','
-                                        {
-                                            continue;
-                                        }
-
-                                        if(data[3] != "")
-                                        {
-                                            soldeInit = decimal.Parse(data[3], CultureInfo.InvariantCulture);
-                                        } else
-                                        {
-                                            soldeInit = 0;
-                                        }
-                                    }
-                                    else
-                                    {
-                                        soldeInit = 0;
-                                    }
-
-
-                                    Compte compte = new Compte(id, idCarte, type, soldeInit);
-                                    dictCarte[idCarte].CompteListe.Add(compte); // Ajout du compte dans la liste des comptes de la carte
-                                    res.Add(id, compte);
-                                }
-                            }
+                            Compte compte = new Compte(compteVerif.Item2, compteVerif.Item3, compteVerif.Item4, compteVerif.Item5);
+                            dictCarte[compteVerif.Item3].CompteListe.Add(compte); // Ajout du compte dans la liste des comptes de la carte
+                            res.Add(compte.IdCompte, compte);
                         }
                     }
-
-
                 }
             }
 
@@ -99,54 +65,19 @@ namespace Banque
                 using (StreamReader reader = new StreamReader(file))
                 {
                     string line;
-                    string[] data;
-                    long id;
-                    int plafond;
-                    List<Compte> compteListe = new List<Compte>();
 
                     while ((line = reader.ReadLine()) != null)
                     {
-                        data = line.Split(';');
+                        (bool, long, int) verification = CarteVerification(line, res);
 
-
-
-                        if (data.Length > 0) // Vérification que l'on a assez de données
+                        if(verification.Item1)
                         {
-
-                            id = long.Parse(data[0]);
-
-                            if (!res.ContainsKey(id) && id.ToString().Length == 16) // Vérification que l'on n'a pas encore le compte et que son id soit assez long
-                            {
-
-                                if (data.Length > 1) // Mise en place de la variable soldeInit
-                                {
-                                    if (data[1] == "")
-                                    {
-                                        plafond = 0;
-                                    }
-                                    else
-                                    {
-                                        plafond = int.Parse(data[1]);
-                                        if (plafond < 500 || plafond > 3000)
-                                        {
-                                            continue;
-                                        }
-                                    }
-                                }
-                                else
-                                {
-                                    plafond = 500;
-                                }
-
-
-                                compteListe = new List<Compte>();
-                                Carte carte = new Carte(id, plafond, compteListe);
-                                res.Add(id, carte);
-                            }
+                            List<Compte> compteListe = new List<Compte>();
+                            compteListe = new List<Compte>();
+                            Carte carte = new Carte(verification.Item2, verification.Item3, compteListe);
+                            res.Add(verification.Item2, carte);
                         }
                     }
-
-
                 }
             }
 
@@ -167,57 +98,189 @@ namespace Banque
                 using (StreamReader reader = new StreamReader(file))
                 {
                     string line;
-                    string[] data;
-                    int id;
-                    DateTime date;
-                    decimal montant;
-                    int expediteur;
-                    int recepteur;
 
                     while ((line = reader.ReadLine()) != null)
                     {
-                        data = line.Split(';');
+                        (bool, int, DateTime, decimal, int, int) verificationTrans = TransactionVerification(line, res);
 
-
-
-                        if (data.Length == 5) // Vérification que l'on a assez de données
+                        if(verificationTrans.Item1)
                         {
-
-                            id = int.Parse(data[0]);
-                            if(DateTime.TryParseExact(data[1], "dd/MM/yyyy HH:mm:ss", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime dateNormalise))
-                            {
-                                date = dateNormalise;
-                            } else
-                            {
-                                continue;
-                            }
-
-                            if (data[2].Contains(','))  // Vérification que le montant initial si il existe, n'a pas de ','
-                            {
-                                continue;
-                            }
-
-                            montant = decimal.Parse(data[2], CultureInfo.InvariantCulture);
-                            expediteur = int.Parse(data[3]);
-                            recepteur = int.Parse(data[4]);
-
-                            if (!res.ContainsKey(id)) // Vérification que l'on n'a pas encore le compte et que son id soit assez long
-                            {
-                                if(montant > 0)
-                                {
-                                    Transaction transac = new Transaction(id, date, montant, expediteur, recepteur);
-                                    res.Add(id, transac);
-
-                                }
-                            }
+                            Transaction transac = new Transaction(verificationTrans.Item2, verificationTrans.Item3, verificationTrans.Item4, verificationTrans.Item5, verificationTrans.Item6);
+                            res.Add(verificationTrans.Item2, transac);
                         }
                     }
-
-
                 }
             }
 
             return res;
+        }
+
+
+        /// <summary>
+        /// Fonction qui va vérifier si l'on peut créer un compte ou non
+        /// </summary>
+        /// <param name="compteData">Un string venant du csv</param>
+        /// <param name="res">Le dictionnaire des comptes</param>
+        /// <returns>Un tuple avec les informations du compte et si l'on peut créer ou non le compte</returns>
+        public (bool, int, long, string, decimal) CompteVerification(string compteData, Dictionary<int, Compte> res)
+        {
+            string[] data;
+            int id;
+            long idCarte;
+            string type;
+            decimal soldeInit;
+
+            data = compteData.Split(';');
+
+            if (data.Length > 2) // Vérification que l'on a assez de données
+            {
+
+                id = int.Parse(data[0]);
+                idCarte = long.Parse(data[1]);
+                type = data[2];
+
+                if (!res.ContainsKey(id) && idCarte.ToString().Length == 16) // Vérification que l'on n'a pas encore le compte et que son id soit assez long
+                {
+                    if (type.ToLower() == "livret" || type.ToLower() == "courant") // Vérification que le type de comtpe soit correct.
+                    {
+
+                        if (data.Length > 3) // Mise en place de la variable soldeInit
+                        {
+                            if (data[3].Contains(',')) //Vérification que le montant initial si il existe, n'a pas de ','
+                            {
+                                
+                                return (false, 0, 0, "", 0);
+                            }
+
+                            if (data[3] != "")
+                            {
+                                soldeInit = decimal.Parse(data[3], CultureInfo.InvariantCulture);
+                            }
+                            else
+                            {
+                                soldeInit = 0;
+                            }
+                        }
+                        else
+                        {
+                            soldeInit = 0;
+                        }
+
+
+
+                        return (true, id, idCarte, type, soldeInit);
+                    }
+                }
+            }
+
+            return (false, 0, 0, "", 0);
+        }
+
+        /// <summary>
+        /// Fonction qui va vérifier si l'on peut créer une carte ou non
+        /// </summary>
+        /// <param name="carteData">Un string venant du csv</param>
+        /// <param name="res">Le dictionaire des cartes</param>
+        /// <returns>Un tuple avec les informations d'une carte et si l'on peut la créer ou non</returns>
+        public (bool, long, int) CarteVerification(string carteData, Dictionary<long, Carte> res)
+        {
+            long id;
+            int plafond;
+            string[] data;
+
+            data = carteData.Split(';');
+
+            if (data.Length > 0) // Vérification que l'on a assez de données
+            {
+
+                id = long.Parse(data[0]);
+
+                if (!res.ContainsKey(id) && id.ToString().Length == 16) // Vérification que l'on n'a pas encore le compte et que son id soit assez long
+                {
+
+                    if (data.Length > 1) // Mise en place de la variable soldeInit
+                    {
+                        if (data[1] == "")
+                        {
+                            plafond = 0;
+                        }
+                        else
+                        {
+                            plafond = int.Parse(data[1]);
+                            if (plafond < 500 || plafond > 3000)
+                            {
+                                return (false, 0, 0);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        plafond = 500;
+                    }
+
+                    return (true, id, plafond);
+                } else
+                {
+
+                    return (false, 0, 0);
+                }
+            } else
+            {
+                return (false, 0, 0);
+            }
+        }
+
+        /// <summary>
+        /// Fonction qui va vérifier si l'on peut créer une transaction ou non
+        /// </summary>
+        /// <param name="transactionData">Un string venant du csv</param>
+        /// <param name="res">Le dictionaire des transactions</param>
+        /// <returns>Un tuple avec les informations d'une transaction et si l'on peut la créer ou non</returns>
+        public (bool, int, DateTime, decimal, int, int) TransactionVerification(string transactionData, Dictionary<int, Transaction> res)
+        {
+            string[] data;
+            int id;
+            DateTime date;
+            decimal montant;
+            int expediteur;
+            int recepteur;
+
+            data = transactionData.Split(';');
+
+
+
+            if (data.Length == 5) // Vérification que l'on a assez de données
+            {
+
+                id = int.Parse(data[0]);
+                if (DateTime.TryParseExact(data[1], "dd/MM/yyyy HH:mm:ss", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime dateNormalise))
+                {
+                    date = dateNormalise;
+                }
+                else
+                {
+                    return (false, 0, DateTime.MinValue ,0,0,0);
+                }
+
+                if (data[2].Contains(','))  // Vérification que le montant initial si il existe, n'a pas de ','
+                {
+                    return (false, 0, DateTime.MinValue, 0, 0, 0);
+                }
+
+                montant = decimal.Parse(data[2], CultureInfo.InvariantCulture);
+                expediteur = int.Parse(data[3]);
+                recepteur = int.Parse(data[4]);
+
+                if (!res.ContainsKey(id)) // Vérification que l'on n'a pas encore le compte et que son id soit assez long
+                {
+                    if (montant > 0)
+                    {
+                        return (true, id, date, montant, expediteur, recepteur);
+
+                    }
+                }
+            }
+            return (false, 0, DateTime.MinValue, 0, 0, 0);
         }
     }
 }
